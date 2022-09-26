7ccaeb205003752fc1dbdb3dd69488462f3b84a1
Kpreciado11 <preciadoblancaskarla@gmail.com>
2022-09-26 15:15:45 -0500

    Se actualizÂaron los cambios

---
+++ scrip.js
@@ -0,0 +1,11 @@
+
+    //aqui generaticon cambia de color
+
+    console.log("Saoko papi saoko");
+    function colorTitulo() {
+      console.log("Pulso el botón de Generation");
+      document.getElementById("generation").style.color = "red";
+    }
+  
+
+ 
